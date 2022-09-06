2e6b854c358338ef14e5d3e6bfd8b3b06fe2456b
kyw0703 <hhh576@naver.com>
2022-09-06 19:44:26 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/bolg/BlogApplication.java
@@ -0,0 +1,13 @@
+package com.cos.bolg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//https://localhost:8080/test/hello
+@SpringBootApplication
+public class BlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogApplication.class, args);
+	}
+
+}
